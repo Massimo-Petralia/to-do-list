8206febd356af1ac878805b14a57d50471cb625c
massimo <massipetra@hotmail.it>
2023-10-07 09:19:16 +0200

    clean-up

--- src/app/features/newItem/newItem.component.ts
+++ src/app/features/newItem/newItem.component.ts
@@ -1,8 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
 import { Item } from 'src/app/models/item';
-import { generateID } from 'src/app/helper/generate-id';
 
 @Component({
   selector: 'app-newItem',
