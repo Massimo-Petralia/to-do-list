d751210c8adff424dd5acd8671757ceca9c7b92c
Riccardo Petralia <petraliariccardo@outlook.it>
2023-10-14 19:16:39 +0200

    feat(page): move page component in to-do-list folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { PageComponent } from './features/page/page.component';
 import { ListComponent } from './features/list/list.component';
 import { ItemComponent } from './features/newItem/newItem.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ToDoItemComponent } from './features/to-do-item/to-do-item.component';
 import { CanvasComponent } from './features/canvas/canvas.component';
+import { PageComponent } from './features/to-do-list/page/page.component';
 
 @NgModule({
   declarations: [
